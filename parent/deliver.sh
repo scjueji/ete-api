5c099023a5ca7a3e238b1af8c53b465712a2dd22
windforce1989 <46036555@qq.com>
2020-01-19 18:46:33 +0800

    11

--- parent/deliver.sh
+++ parent/deliver.sh
@@ -5,9 +5,10 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
  
-sh "chmod 777 ./parent/ete_server/pom.xml"
+
  mvn clean  -f ./parent/pom.xml
+ mvn clean  install -f ./parent/common/pom.xml
  mvn clean install -f ./parent/ete_server/pom.xml
 
-sh "ls -l"
+
 java -jar target/eteserver-1.0-SNAPSHOT.jar
