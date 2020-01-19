389bdd90ffc181c6b1f4daf689884b03a4d07246
windforce1989 <46036555@qq.com>
2020-01-19 18:29:11 +0800

    11

--- parent/deliver.sh
+++ parent/deliver.sh
@@ -4,10 +4,10 @@ echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
- mvn clean install -f ./parent/pom.xml
- mvn clean install -f ./parent/common/pom.xml
- mvn clean install -f ./parent/ete_getway/pom.xml
+ 
+
+ 
  mvn clean install -f ./parent/ete_server/pom.xml
- mvn clean install -f ./parent/seat_provider/pom.xml
+
 
 java -jar target/eteserver-1.0-SNAPSHOT.jar
