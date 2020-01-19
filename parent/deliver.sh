95c2f4d61811dac1ea5a05134cc1d82955c1df9f
windforce1989 <46036555@qq.com>
2020-01-19 18:05:10 +0800

    11

--- parent/deliver.sh
+++ parent/deliver.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set +x
+echo 'The following Maven command installs your Maven-built Java application'
+echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
  mvn clean install -f ./pom.xml
  mvn clean install -f ./common/pom.xml
  mvn clean install -f ./ete_getway/pom.xml
