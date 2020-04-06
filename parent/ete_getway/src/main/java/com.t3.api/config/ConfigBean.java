c296565a44bf6de4e1ffaed8af311fdbb9fcf844
scjueji <1163697772@qq.com>
2020-04-06 22:15:12 +0800

    优化redis客户端连接代码，新增redistemplate序列化工具

--- parent/ete_getway/src/main/java/com.t3.api/config/ConfigBean.java
+++ parent/ete_getway/src/main/java/com.t3.api/config/ConfigBean.java
@@ -3,6 +3,9 @@ package com.t3.api.config;
 import com.t3.api.filter.AuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 /**
  * Created by Administrator on 2019/12/30.
@@ -18,4 +21,14 @@ public class ConfigBean {
     public PropertyConfig propertyConfig(){
         return  new PropertyConfig();
     }
+
+
+    @Bean
+    public StringRedisTemplate myredisTemplate(RedisConnectionFactory redisConnectionFactory){
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setHashKeySerializer(new Jackson2JsonRedisSerializer(Object.class));
+
+        return template;
+    }
+
 }
